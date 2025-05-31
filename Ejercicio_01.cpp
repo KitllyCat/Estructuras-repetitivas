0b09cee19a98fa526091774d459845427fe053a5
KitllyCat <cjchambem@unjbg.edu.pe>
2025-05-31 18:30:13 -0500

    Se añadio un bucle para comprobar si el numero es 0 o menor

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -5,19 +5,22 @@ int main(){
 	
 	int n,multiplo=7,i=1;
 	
-	cout<<"Ingrese la cantidad de 'n' multiplos que desea saber de 7: ";
-	cin>>n;
+	do{
+		cout<<"Ingrese la cantidad de 'n' multiplos que desea saber de 7: ";
+		cin>>n;
 	
-	if(n<=0){
-		cout<<"El valor no puede ser cero."<<endl;	
-	}else {
+		if(n<=0){
+			cout<<"El valor no puede ser cero."<<endl;
+		}
+	}while (n<=0);
+	
 		cout << "Los primeros " << n << " multiplos de 7 son:" << endl;
+		
 		while (i<=n){
 			cout<<"-> "<<multiplo<<endl;
 			multiplo+=7;
 			i++;
 		}
-	}
 	cout<<"Fin."<<endl;
 	return 0;
 }
