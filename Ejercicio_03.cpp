b48eb6e85797487fe59c33e2ef86d3553d846018
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-01 19:40:34 -0500

    Se añadio un bucle que comprueba si el valor es 0 o menor

--- Ejercicio_03.cpp
+++ Ejercicio_03.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main(){
 	int num,sumT=0,x=0,y=0,z;
-	cout<<"Ingrese la cantidad de 'n' numeros que desea sumar: ";
-	cin>>num;
+	do{
+		cout<<"Ingrese la cantidad de 'n' numeros que desea sumar: ";
+		cin>>num;
+		
+		if(num<=0){
+			cout<<"El numero no puede ser cero o menor"<<endl;
+		}
+	}while(num<=0);
 	
 	for(int i=0;i<num;i++){
 		y++;
